1d41354747847929dbc0094365372a0f58bb13c7
ws0h3ll <45339471+ws0h3ll@users.noreply.github.com>
2019-04-23 20:48:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 from flask import Flask
 
-UPLOAD_FOLDER = 'C:/Users/Visitor/uploads'
+UPLOAD_FOLDER = 'where to save to'
 app = Flask(__name__)
-app.secret_key = os.urandom(16)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024 * 1024
+app.secret_key = os.urandom(16) #encryption
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER #upload folder
+app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024 * 1024 #maximum file size (i set mine to 200Gb)
