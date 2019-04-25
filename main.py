09279cc73357b294d87976500453581e05da1562
ws0h3ll <45339471+ws0h3ll@users.noreply.github.com>
2019-04-25 18:44:44 +0100

    Update main.py
    deletes all files after an hour

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
-import os
+import os, glob, time, operator, stat, threading, time
 from app import app
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif']) #file extensions that are allowed
 
+
+
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS #check if the file extensions is in the allowed files array
+
+@app.before_first_request
+def activate_job():
+	def check_files():
+		while True:
+			dir = str(app.config['UPLOAD_FOLDER'])
+			files = glob.glob(dir+'/*')
+			print(files)
+			for file in files:
+				st = os.stat(file)
+				age =(time.time()-st.st_mtime) / 3600
+				if age > 1:
+					os.remove(file)
+					print('removed')
+				else:
+					print(age)
+				time.sleep(2)
+	thread = threading.Thread(target=check_files)
+	thread.start()
+@app.route('/index')
+def index():
+	return render_template('index.html')
 
 @app.route('/') #index
 def upload_form():
@@ -22,10 +46,15 @@ def upload_file():
 			flash('select file to upload')
 			return redirect(request.url) # return home
 		if file and allowed_file(file.filename): #if it is a valid file
+			print(allowed_file(file.filename))
 			filename = secure_filename(file.filename)
 			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) #save it to the server
 			flash('File(s) successfully uploaded') #tell user it was a success
 			return redirect('/')
+		else:
+			return 'Not allowed'
 
 if __name__ == "__main__":
-    app.run(debug=True,port=80) # run it
+	while 1:
+
+		app.run(debug=True) # run it
